f39a484243a9a99bc72e80ae5ae6afc8a8f0832b
Davain Pablo Edwards <core8@gmx.net>
2019-04-14 20:42:11 +0200

    :star: add constants

---
+++ src/com/dpedwards/blockchain/Constants.java
@@ -0,0 +1,20 @@
+package com.dpedwards.blockchain;
+
+   /**
+    * Used to define main constants for blockchain 
+    * 
+    * @author Davain Pablo Edwards
+    * @license MIT 
+    * @version 1.0
+    */
+public class Constants {
+	
+	private Constants() {
+		
+	}
+	
+	public static final int DIFFICULTY = 2;
+	public static final double MINER_REWARD = 11;
+	public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
+
+}
